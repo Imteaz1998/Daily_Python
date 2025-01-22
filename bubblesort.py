55e61f2b5a5482b818de9779862c3ee57bd996fd
Imteaz1998 <ahmedimteaz073@gmail.com>
2025-01-22 12:51:53 +0600

    Bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,13 @@
-print("Hello world")
+def bubblesort(arr):
+    n=len(arr)
+    for passes in range(0,n):
+        for j in range(0,n-1):
+            if(arr[j]>arr[j+1]):
+                arr[j],arr[j+1]=arr[j+1],arr[j]
+    return arr
+
+
+
+unsorted_list=[10,90,85,64,33]
+sorted_list=bubblesort(unsorted_list)
+print(sorted_list)
